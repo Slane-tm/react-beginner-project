e28ea1ae564c2eef3b840a36e95f1f22d63ededf
Slane-tm <sjorageldiyev@gmail.com>
2023-02-02 12:20:41 +0500

    change README.md

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react'
 import './index.scss';
 
 function App() {
+  const [count, setState] = React.useState(0)
+
+  function incCount() {
+    setState(count + 1)
+  }
+
+  function decCount() {
+    setState(count - 1)
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={decCount} className="minus">- Минус</button>
+        <button onClick={incCount} className="plus">Плюс +</button>
       </div>
     </div>
   );
